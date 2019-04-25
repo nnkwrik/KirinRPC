d8e77870f978c17fdd764f5c6eaa602e4b3a0c8d
nnkwrik <nnkwrik@gmail.com>
2019-04-25 11:36:38 +0900

    first commit

---
+++ kirinrpc-demo/src/main/java/io/github/nnkwrik/kirinrpc/demo/api/HelloWorldService.java
@@ -0,0 +1,9 @@
+package io.github.nnkwrik.kirinrpc.demo.api;
+
+/**
+ * @author nnkwrik
+ * @date 19/04/25 11:31
+ */
+public interface HelloWorldService {
+    String sayHello(String name);
+}
---
+++ kirinrpc-demo/src/main/java/io/github/nnkwrik/kirinrpc/demo/provider/ProviderDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.nnkwrik.kirinrpc.demo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderDemoApplication.class, args);
+    }
+
+}
